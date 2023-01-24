01f52a23b755378b7f12140a95c9eb116b53bba9
Luth Andyka <luthandyka.business@gmail.com>
2023-01-24 22:44:41 +0800

    add progress indicator at favourite movie list activity

--- app/src/main/java/nyp/sit/movieviewer/advanced/ui/activity/FavouriteMovieListActivity.kt
+++ app/src/main/java/nyp/sit/movieviewer/advanced/ui/activity/FavouriteMovieListActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.activity_movie_list.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import nyp.sit.movieviewer.advanced.databinding.ActivityFavouriteMovieListBinding
 import nyp.sit.movieviewer.advanced.entity.Movie
 import nyp.sit.movieviewer.advanced.ui.FavouriteMovieAdapter
@@ -21,7 +25,21 @@ class FavouriteMovieListActivity : AppCompatActivity() {
         binding = ActivityFavouriteMovieListBinding.inflate(layoutInflater)
         adapter = FavouriteMovieAdapter(this)
 
+        val j = lifecycleScope.launch {
+            var progress = binding.movieListProgressIndicator.progress
+            while (progress < 100) {
+                delay(500)
+                if (progress == 100) {
+                    return@launch
+                }
+                progress += 5
+                binding.movieListProgressIndicator.setProgressCompat(progress, true)
+            }
+        }
+
         val moviesObserver = Observer<List<Movie>> {
+            j.cancel()
+            binding.movieListProgressIndicator.setProgressCompat(100, true)
             adapter.clear()
             adapter.addAll(it)
         }
